7a6e4bb05f96a92ffd5af99ae846f93455063850
Sachin Parmar <sachinparmar758@gmail.com>
2026-05-26 12:29:45 +0530

    API fixes

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,57 @@
+const User = require("../models/user.model");
+const { hashPassword, comparePassword } = require("../utils/hash");
+const generateToken = require("../utils/jwt");
+
+exports.signup = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = generateToken({ id: user._id, role: user.role });
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
